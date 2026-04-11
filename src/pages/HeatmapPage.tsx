e81337d1383ba0afab21d67795b3698fbfb40a13
Nischal Singana <singananischal@gmail.com>
2026-04-11 10:43:47 +0530

    style: revert to high-color OSM mapping utilizing advanced CSS color-rotation saturation filters for vibrant dark mode

--- src/pages/HeatmapPage.tsx
+++ src/pages/HeatmapPage.tsx
@@ -146,7 +146,7 @@ export function HeatmapPage() {
                 <ZoomControl position="bottomright" />
                 <TileLayer
                   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                  url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
                 {dynamicBounds && <AutoFitBounds bounds={dynamicBounds} />}
                 
